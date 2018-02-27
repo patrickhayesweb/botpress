508f2ae91f9d8201ff31b76f498b3bb0987a2433
Eugene Mirotin <emirotin@gmail.com>
2018-02-27 03:07:09 +0300

    implement uploading & fixes

--- src/web/components/ContentForm/UploadWidget.js
+++ src/web/components/ContentForm/UploadWidget.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
-import { Button, FormGroup, InputGroup, FormControl } from 'react-bootstrap'
+import { Button, FormGroup, InputGroup, FormControl, HelpBlock } from 'react-bootstrap'
+import Loading from '~/components/Util/Loading'
+import axios from 'axios'
 
 class UploadWidget extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      expanded: !props.value
+      expanded: !props.value,
+      error: null,
+      uploading: false
     }
   }
 
@@ -16,8 +20,8 @@ class UploadWidget extends Component {
     }
   }
 
-  handleChange = filename => {
-    this.props.onChange(filename)
+  handleChange = url => {
+    this.props.onChange(url)
   }
 
   expand = () => {
@@ -28,7 +32,31 @@ class UploadWidget extends Component {
     this.setState({ expanded: false })
   }
 
-  startUpload = () => {}
+  onPickerChange = e => {
+    const data = new FormData()
+    data.append('file', event.target.files[0])
+    this.setState({ uploadData: data })
+  }
+
+  startUpload = e => {
+    const { uploadData } = this.state
+
+    this.setState({ error: null, uploading: true }, () => {
+      axios
+        .post('/media', uploadData, { headers: { 'Content-Type': 'multipart/form-data' } })
+        .then(response => {
+          this.setState({ expanded: false })
+          const { url } = response.data
+          this.handleChange(url)
+        })
+        .catch(e => {
+          this.setState({ error: e.message })
+        })
+        .then(() => {
+          this.setState({ uploading: false, uploadData: null })
+        })
+    })
+  }
 
   render() {
     const { $filter: filter, $subtype: subtype, type } = this.props.schema
@@ -36,43 +64,57 @@ class UploadWidget extends Component {
       return null
     }
 
-    const { expanded } = this.state
+    const { expanded, uploading, error, uploadData } = this.state
 
     return (
       <div>
         <FormGroup>
-          <InputGroup>
-            <FormControl
-              type="text"
-              placeholder="No file picket yet"
-              value={this.props.value}
-              disabled
-              className="form-control"
-              id={this.props.id}
-            />
-            <InputGroup.Button>
-              {!expanded ? (
-                <Button onClick={this.expand}>{this.props.value ? 'Change...' : 'Pick...'}</Button>
-              ) : (
-                <Button onClick={this.collapse}>Cancel</Button>
-              )}
-            </InputGroup.Button>
-          </InputGroup>
-        </FormGroup>
-        {expanded && (
-          <FormGroup>
+          {!expanded ? (
             <InputGroup>
               <FormControl
-                type="file"
-                accept={filter || '*'}
-                placeholder={this.props.placeholder || 'Pick file to upload'}
+                type="text"
+                placeholder="No file picked yet"
+                value={this.props.value}
+                disabled
+                className="form-control"
+                id={this.props.id}
               />
+
               <InputGroup.Button>
-                <Button bsColor="success">Upload...</Button>
+                <Button onClick={this.expand}>{this.props.value ? 'Change...' : 'Pick...'}</Button>
               </InputGroup.Button>
             </InputGroup>
+          ) : (
+            <HelpBlock>No file picked yet.</HelpBlock>
+          )}
+        </FormGroup>
+        {expanded &&
+          !uploading && (
+            <FormGroup>
+              <InputGroup>
+                <FormControl
+                  type="file"
+                  accept={filter || '*'}
+                  placeholder={this.props.placeholder || 'Pick file to upload'}
+                  onChange={this.onPickerChange}
+                />
+                <InputGroup.Button>
+                  <Button bsStyle="success" onClick={this.startUpload} disabled={!uploadData}>
+                    Upload...
+                  </Button>
+                  <Button bsStyle="link" onClick={this.collapse}>
+                    Cancel
+                  </Button>
+                </InputGroup.Button>
+              </InputGroup>
+            </FormGroup>
+          )}
+        {error && (
+          <FormGroup validationState="error">
+            <HelpBlock>{error}</HelpBlock>
           </FormGroup>
         )}
+        {expanded && uploading && <Loading />}
       </div>
     )
   }
